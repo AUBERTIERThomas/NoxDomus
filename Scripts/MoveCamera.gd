1fa912e36e2b8f7e44ec0b45bdff88269dabbe20
AUBERTIERThomas <t.aubertier@gmail.com>
2025-02-05 11:51:22 +0100

    un menu ?

--- Scripts/MoveCamera.gd
+++ Scripts/MoveCamera.gd
@@ -1,28 +1,31 @@
 extends Camera3D
 
-var rot_speed = 0.02
+var rot_speed = 0.03
 var mov_speed = 10
 var acceleration = 25.0
+var dim_salle = 9
 
 var velocity = Vector3.ZERO
-var lookAngles = Vector2.ZERO
-# Called when the node enters the scene tree for the first time.
+var lookAngles_y = 0 #Vector2.ZERO
+#Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
 func _process(delta):
-	lookAngles.y = clamp(lookAngles.y, -PI/2, PI/2)
-	set_rotation(Vector3(lookAngles.x, lookAngles.y, 0))
-	var direction = updateDirection()
 	if Input.is_action_pressed("ui_page_up"):
-		self.rotate_y(rot_speed)
+		lookAngles_y += rot_speed
 	if Input.is_action_pressed("ui_page_down"):
-		self.rotate_y(-rot_speed)
+		lookAngles_y -= rot_speed
+	#lookAngles_y = clamp(lookAngles_y, -PI, PI)
+	set_rotation(Vector3(0, lookAngles_y, 0))
+	var direction = updateDirection()
 	if direction.length_squared() > 0:
 		velocity += direction * acceleration * delta
 	if velocity.length() > mov_speed:
 		velocity = velocity.normalized() * mov_speed
 	translate(velocity * delta)
+	position.x = clamp(position.x, -dim_salle, dim_salle)
+	position.z = clamp(position.z, -dim_salle, dim_salle)
 
 func updateDirection() :
 	var dir = Vector3()
